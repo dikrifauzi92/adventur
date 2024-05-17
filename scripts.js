d37d251640af6110ea6d13c11d0a93ed6a62968a
dikrifauzi92 <dikrifauzi92@gmail.com>
2024-05-18 01:45:37 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        alert("Message sent successfully!");
+    });
+});
